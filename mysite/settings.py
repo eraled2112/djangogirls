eb77c630ca712dc063e7d23b9eb09e096e0eec0b
Albina <erkinbekovaalbinaedilbekovna@gmail.com>
2024-04-19 10:41:56 +0600

    Second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-umg%aidj9+lx66qrb%#$j1gc4!&bw_plq$bx@gj5n*c)l&h-tr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
